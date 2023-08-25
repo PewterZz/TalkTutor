83408e6e9a42d81e20cb676ffae80b78a13311c3
PewterZz <Peterns1609@gmail.com>
2023-08-25 17:19:16 +0700

    organizing

---
+++ lib/project/lessons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LessonsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Lessons Page'),
+    );
+  }
+}
